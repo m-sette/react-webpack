45fed3b06edb082b120db111fb69ba992dd9c8a5
Matte Sette <msette8@gmail.com>
2020-12-07 22:28:16 +0100

    backend

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setNotes(res.data)
+    })
+  }, [url])
+  return notes
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([])
+  const notes = useNotes(BACKEND_URL)
+
   const handleClick = () => {
     setCounter(counter + 1)
-    setValues(values.concat(counter))  }
+    setValues(values.concat(counter))
+  }
 
   return (
-    <div className="container">
+    <div className='container'>
       hello webpack {counter} clicks
-      <button onClick={handleClick}>
-        press
-      </button>
+      <button onClick={handleClick}>press</button>
+      <div>
+        {notes.length} notes on server {BACKEND_URL}
+      </div>
     </div>
   )
 }
